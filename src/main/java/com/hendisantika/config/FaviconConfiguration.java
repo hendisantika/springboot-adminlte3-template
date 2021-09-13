f1e55d5061836b05fda2ec2cb1abbe42ca41045b
Hendi Santika <hendisantika@gmail.com>
2021-09-13 07:09:56 +0700

    #18 - Add FaviconConfiguration

---
+++ src/main/java/com/hendisantika/config/FaviconConfiguration.java
@@ -0,0 +1,46 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : adminlte3-sample
+ * User: TOSHIBA
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/13/2021
+ * Time: 6:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class FaviconConfiguration {
+
+    @Bean
+    public SimpleUrlHandlerMapping customFaviconHandlerMapping() {
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setOrder(Integer.MIN_VALUE);
+        mapping.setUrlMap(Collections.singletonMap(
+                "/favicon.ico", faviconRequestHandler()));
+        return mapping;
+    }
+
+    @Bean
+    protected ResourceHttpRequestHandler faviconRequestHandler() {
+        ResourceHttpRequestHandler requestHandler
+                = new ResourceHttpRequestHandler();
+        ClassPathResource classPathResource
+                = new ClassPathResource("com/baeldung/images/");
+        List<Resource> locations = Arrays.asList(classPathResource);
+        requestHandler.setLocations(locations);
+        return requestHandler;
+    }
+}
