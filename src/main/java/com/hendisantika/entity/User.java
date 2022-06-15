b407da9bb6a15f112b429c365d6957223730ff73
Hendi Santika <hendisantika@yahoo.co.id>
2022-06-15 14:05:54 +0700

    #47 - Add User Entity

---
+++ src/main/java/com/hendisantika/entity/User.java
@@ -0,0 +1,35 @@
+package com.hendisantika.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-adminlte3-template
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/15/22
+ * Time: 13:58
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "t_user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    private String password;
+}
