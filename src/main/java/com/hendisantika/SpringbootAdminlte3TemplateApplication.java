e17724e883acf5551329086c08c80c890c49e34f
hendisantika <hendisantika@yahoo.co.id>
2023-09-05 09:35:24 +0700

    #116 - Add Init User Data ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/SpringbootAdminlte3TemplateApplication.java
+++ src/main/java/com/hendisantika/SpringbootAdminlte3TemplateApplication.java
@@ -1,7 +1,11 @@
 package com.hendisantika;
 
+import com.hendisantika.entity.User;
+import com.hendisantika.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootAdminlte3TemplateApplication {
@@ -10,4 +14,14 @@ public class SpringbootAdminlte3TemplateApplication {
         SpringApplication.run(SpringbootAdminlte3TemplateApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demo(UserRepository userRepository) {
+        return (args) -> {
+            userRepository.save(new User(1L, "hendi", "hendi@yopmail.com", "hendi123"));
+            userRepository.save(new User(2L, "kakashi", "kakashi@yopmail.com", "hendi123"));
+            userRepository.save(new User(3L, "naruto", "naruto@yopmail.com", "hendi123"));
+            userRepository.save(new User(4L, "sasuke", "sasuke@yopmail.com", "hendi123"));
+            userRepository.save(new User(5L, "sakura", "sakura@yopmail.com", "hendi123"));
+        };
+    }
 }
